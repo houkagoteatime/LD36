d346681e088b894fcecb52c5fbe0da8ab3e54888
J0hnCena <joshua99wong@gmail.com>
2016-08-26 20:24:01 -0500

    Make basic game

---
+++ src/com/github/houkagoteatime/LD36/LD36Game.java
@@ -0,0 +1,53 @@
+package com.github.houkagoteatime.LD36;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class LD36Game extends Game{
+	
+	private SpriteBatch batch;
+	private BitmapFont font;
+	public static void main(String args[]) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Harambe 2: Origin Stories";
+		config.width = 600;
+		config.height = 1000;
+		new LwjglApplication(new LD36Game(), config);
+	}
+	
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+	}
+	
+	/**
+	 * @return the batch
+	 */
+	public SpriteBatch getBatch() {
+		return batch;
+	}
+	/**
+	 * @param batch the batch to set
+	 */
+	public void setBatch(SpriteBatch batch) {
+		this.batch = batch;
+	}
+	/**
+	 * @return the font
+	 */
+	public BitmapFont getFont() {
+		return font;
+	}
+	/**
+	 * @param font the font to set
+	 */
+	public void setFont(BitmapFont font) {
+		this.font = font;
+	}
+
+
+}
